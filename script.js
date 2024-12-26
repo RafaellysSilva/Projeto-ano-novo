2d57f56170543e978b50b37fcec5c64bf96e148c
RafaellysSilva <162441877+RafaellysSilva@users.noreply.github.com>
2024-12-26 02:39:15 -0300

    .
    ele só entrar no clique se a imagem estiver aumentada no computador, se não, nao entra

--- script.js
+++ script.js
@@ -17,6 +17,11 @@ const cnv = document.getElementById("canvas");
   const texto3 = "Ela tem algo a te dizer, clique no coração e"
   const texto4 = "descubra!"
 
+const coracaoX = 325;
+const coracaoY = 730;
+const coracaoWidth = 200; // Largura do coração
+const coracaoHeight = 200; // Altura do coração
+
   function balaoFala(){
     ctx.fillStyle = "#c1e678"
     ctx.fillRect(600 , 450 , 350 , 100) //x , y , largura , altura
@@ -26,9 +31,24 @@ const cnv = document.getElementById("canvas");
     ctx.fillText(texto2 , 605 , 490)
     ctx.fillText(texto3 , 605 , 510)
     ctx.fillText(texto4 , 605 , 530)
-    let heart = ctx.drawImage(coracao , 390 , 900 , 200 , 200)
+    let coracaoImg = ctx.drawImage(coracao , 390 , 900 , 200 , 200)
+    console.log(coracao.getWidth)
   }
   
+function verificarClique(event) {
+  const mouseX = event.offsetX;
+  const mouseY = event.offsetY;
+  console.log("mouseX: " + mouseX +" mouseY: " + mouseY)
+
+  // Verificar se o clique ocorreu dentro da área do coração (simulando o clique na imagem)
+  if (mouseX > coracaoX && mouseX < coracaoX + coracaoWidth && mouseY > coracaoY && mouseY < coracaoY + coracaoHeight) {
+    // Redireciona para o YouTube
+    window.location.href = 'https://www.youtube.com'; // Substitua com o link desejado
+  }
+}
+
+  // Adiciona o evento de clique ao canvas
+cnv.addEventListener('click', verificarClique);
 
   // Aguarde a imagem carregar antes de desenhá-la
   img.onload = function () {
